a17db890a76de19c20b890bf70b76393e26a1a70
Attila Jones <attila.g.jones@gmail.com>
2021-09-15 18:41:41 -0400

    proximity summary with RMSD completed

--- src/repos_tools.py
+++ src/repos_tools.py
@@ -7,22 +7,16 @@ def drop_genes_notin_network(genes, network):
     dropped_genes = set(genes).difference(set(kept_genes))
     return((kept_genes, dropped_genes))
 
-def plot_proximity_results(prox):
+def plot_proximity_results(prox, jitter=True):
     fig, ax = plt.subplots(1, 3, sharey=True, figsize=(8, 4))
-    sns.stripplot(x='d', y='target', hue='condition', data=prox, ax=ax[0])
-    sns.stripplot(x='z', y='target', hue='condition', data=prox, ax=ax[1])
-    sns.stripplot(x='p', y='target', hue='condition', data=prox, ax=ax[2])
+    for quantity, axi in zip(list('dzp'), ax):
+        sns.stripplot(x=quantity, y='target', hue='condition', data=prox, ax=axi, jitter=jitter)
+        axi.get_legend().remove()
+        axi.grid(True, axis='y')
     ax[2].set_xscale('log')
-    ax[0].get_legend().remove()
-    ax[1].get_legend().remove()
-    ax[2].get_legend().remove()
     ax[0].set_xlabel('distance')
     ax[1].set_xlabel('z-score')
     ax[2].set_xlabel('p-value')
-    ax[0].grid(True, axis='y')
-    ax[1].grid(True, axis='y')
-    ax[2].grid(True, axis='y')
     handles, labels = ax[0].get_legend_handles_labels()
     fig.legend(handles, labels, loc='upper center', ncol=2)
-    #fig.legend(handles, labels, loc='upper center', ncol=2, title='AD genes')
     return((fig, ax))
