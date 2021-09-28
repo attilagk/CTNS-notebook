ee7af7b92e4739d32f16864e2bcb354eaed1d69e
Attila Jones <attila.g.jones@gmail.com>
2021-09-28 09:46:24 -0400

    after failed run dumping raw results with pickle and another try statement

--- src/proximity.py
+++ src/proximity.py
@@ -4,10 +4,12 @@ import os
 import pandas as pd
 import numpy as np
 import time
+import datetime
 from toolbox import wrappers
 import concurrent.futures
 import repos_tools
 from scipy import stats
+import pickle
 
 
 main_dirpath = '../../'
@@ -45,7 +47,9 @@ def calculate_proximities(drugbank_prot,
                           network_fpath=main_dirpath + 'resources/PPI/Cheng2019/network.sif',
                           id_mapping_file=main_dirpath + 'resources/PPI/geneid_to_symbol.txt',
                           drugbank_all_drugs_fpath=main_dirpath + 'results/2021-08-11-drugbank/drugbank-all-drugs.csv',
-                          asynchronous=True, max_workers=os.cpu_count() - 1):
+                          asynchronous=True,
+                          pickle_path='default',
+                          max_workers=os.cpu_count() - 1):
     '''
     '''
     start = time.time()
@@ -62,6 +66,10 @@ def calculate_proximities(drugbank_prot,
             val = list(executor.map(process_drug, l))
     else:
         val = list(map(process_drug, l)) # for testing synchronous execution
+    if pickle_path == 'default':
+        pickle_path = '/tmp/' + datetime.datetime.utcnow().isoformat() + '.p'
+    print('dumping results to', pickle_path)
+    pickle.dump(val, open(pickle_path, 'wb'))
     dfval = postprocess_prox_results(val, drugbank_prot, drugbank_all_drugs_fpath)
     runtime = time.time() - start
     print(f'total runtime: {runtime:.1f}s')
@@ -70,9 +78,12 @@ def calculate_proximities(drugbank_prot,
 
 def postprocess_prox_result(inval):
     drugbank_id, prox_res = inval
-    d, z, H0 = prox_res
-    avg_d_H0, sdev_d_H0 = H0
-    p = stats.norm.sf(-z)
+    try:
+        d, z, H0 = prox_res
+        avg_d_H0, sdev_d_H0 = H0
+        p = stats.norm.sf(-z)
+    except:
+        d, avg_d_H0, sdev_d_H0, z, p = (np.nan, ) * 5
     d = {'d': d, 'avg_d_H0': avg_d_H0, 'sdev_d_H0': sdev_d_H0, 'z': z, 'p': p}
     ix = pd.Index([drugbank_id], name='drugbank_id')
     val = pd.DataFrame(d, index=ix)
@@ -107,6 +118,7 @@ if __name__ == '__main__':
                                    id_mapping_file=config['DEFAULT']['id_mapping_file'],
                                    drugbank_all_drugs_fpath=config['DEFAULT']['drugbank_all_drugs_fpath'],
                                    asynchronous=config.getboolean('DEFAULT', 'asynchronous'),
+                                   pickle_path=config['DEFAULT']['out_csv'] + '.p',
                                    max_workers=config.getint('DEFAULT', 'max_workers'))
     result.to_csv(config['DEFAULT']['out_csv'])
     print('Results written to', config['DEFAULT']['out_csv'])
